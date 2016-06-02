caecb7ce11dc91ea5db2e6eb92633a4912db56d1
badeleux <badeleux@gmail.com>
2016-06-02 14:25:35 +0300

    Initial commit

---
+++ index.markdown
@@ -0,0 +1,11 @@
+# [ن](https://www.i-am-n.com)
+
+I'm **badeleux**.
+
+Developer from Poland.
+
+You can contact me at [twitter](https://twitter.com/badeleux) & [badeleux@gmail.com](mailto:badeleux@gmail.com)
+
+My recent projects: [UFL](http://ultimatefanlive.com/), [Eventheon](https://github.com/Eventheon)
+
+Languages I use: _Haskell_, _Objective-C_, _Swift_
---
+++ site.hs
@@ -0,0 +1,28 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "fonts/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "index.markdown" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
